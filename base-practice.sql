5cd062b284c7bfaa464c93bc2725669aa159c79b
zllisme <1973842936@qq.com>
2019-06-23 20:46:38 +0800

    finish

--- base-practice.sql
+++ base-practice.sql
@@ -3,24 +3,26 @@ create database student_examination_sys;
 use student_examination_sys;
 
 create table student (
-    id varchar(4),
-	name varchar(10),
-	age int,
-	sex char(2)
+    id varchar(4) PRIMARY KEY,
+    name varchar(10),
+    age int,
+    sex char(2)
 );
 
 create table subject (
-    id varchar(5),
-	sucject varchar(10),
-	teacher varchar(10),
-	description varchar(30)
+    id varchar(5) PRIMARY KEY,
+    sucject varchar(10),
+    teacher varchar(10),
+    description varchar(30)
 );
 
 create table score (
-    id varchar(5),
-	student_id varchar(4),
-	subject_id varchar(5),
-	score DECIMAL(4,1)
+    id varchar(5) PRIMARY KEY,
+    student_id varchar(4),
+    subject_id varchar(5),
+    score DECIMAL(4,1),
+    CONSTRAINT score_student_fore FOREIGN KEY (student_id) REFERENCES student (id),
+    CONSTRAINT score_subject_fore FOREIGN KEY (subject_id) REFERENCES subject (id)
 );
 
 insert into student values 
