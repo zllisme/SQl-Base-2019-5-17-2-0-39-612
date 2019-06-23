45aa86b42ce9a73553b29b5ae85f4cd0da3045b6
zllisme <1973842936@qq.com>
2019-06-23 18:18:26 +0800

    finish practice

---
+++ base-practice.sql
@@ -0,0 +1,38 @@
+create database student_examination_sys;
+
+use student_examination_sys;
+
+create table student (
+    id varchar(4),
+	name varchar(10),
+	age int,
+	sex char(2)
+);
+
+create table subject (
+    id varchar(5),
+	sucject varchar(10),
+	teacher varchar(10),
+	description varchar(30)
+);
+
+create table score (
+    id varchar(5),
+	student_id varchar(4),
+	subject_id varchar(5),
+	score DECIMAL(4,1)
+);
+
+insert into student values 
+('001','张三',18,'男'),
+('002','李四',20,'女');
+
+insert into subject values 
+('1001','语文','王老师','本次考试比较简单'),
+('1002','数学','刘老师','本次考试比较难');
+
+insert into score values 
+('1','001','1001',80),
+('2','002','1002',60),
+('3','001','1001',70),
+('4','002','1002',60.5);
